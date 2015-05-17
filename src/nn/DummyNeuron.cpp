af75e2066d5267066c7dcdec5140a61d977c5eb0
Nicki Ogg <oggnicki@yahoo.com>
2015-05-17 18:12:55 +0200

    DummyNeuron

---
+++ src/nn/DummyNeuron.cpp
@@ -0,0 +1,14 @@
+#include "DummyNeuron.h"
+
+/**
+ * This method does nothing
+ */
+void DummyNeuron::computeValue() {
+	// Do nothing
+}
+/**
+ * This method always returns -1
+ */
+double DummyNeuron::getValue() {
+	return -1;
+}
--- src/nn/DummyNeuron.h
+++ src/nn/DummyNeuron.h
@@ -1,10 +1,23 @@
 #include <vector>
 #include <math.h>
+#include "Neuron.h"
 
 #ifndef dummy_neuron_header
 
+/**
+ * A DummyNeuron always returns a value of -1
+ * This is used to act as a threshold for other neurons
+ */
 class DummyNeuron : public Neuron {
-
+	public:
+		/**
+		 * This method does nothing
+		 */
+		void computeValue();
+		/**
+		 * This method always returns -1
+		 */
+		double getValue();
 };
 
 #define dummy_neuron_header
