51486b61f7c9b83082dffdd56a37df9c6d925d93
Nicki Ogg <oggnicki@yahoo.com>
2015-05-17 17:48:36 +0200

    Neuron header

--- src/nn/Neuron.h
+++ src/nn/Neuron.h
@@ -1,12 +1,49 @@
 #include <string>
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 #ifndef neuron_header
 
 class Neuron {
 	public:
-	private:
+		/**
+		 * Make the neuron compute its value
+		 */
+		void computeValue();
+
+		/**
+		 * Get the neuron's value
+		 * This does not update automatically, computeValue must be called to refresh the value
+		 */
+		double getValue();
+
+		/**
+		 * Set the neuron's layer
+		 */
+		void setLayer(int layer);
+
+		/**
+		 * Set the weights of the neuron
+		 * This replaces the current weights of the neuron
+		 */
+		void setWeights(std::vector <double> weights);
+
+		/**
+		 * Get the neuron's current weights
+		 */
+		std::vector <double> getWeights();
+
+		/**
+		 * Set the neuron's previous layer
+		 */
+		void setPreviousLayer(std::vector <Neuron> previous_layer);
+
+
+	protected:
+		double *weights;
+		double value;
+		std::vector <Neuron> previous_layer;
 };
 
 #define neuron_header
