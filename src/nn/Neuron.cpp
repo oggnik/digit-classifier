42e5d0660302fc61cde97d8e1ed2c0e6b493851d
Nicki Ogg <oggnicki@yahoo.com>
2015-05-19 21:51:53 +0200

    test uninitialized

--- src/nn/Neuron.cpp
+++ src/nn/Neuron.cpp
@@ -142,12 +142,11 @@ double Neuron::derivativeInputValue() {
 	// Add up inputs * weights
 	int previous_layer_size = weights.size();
 	for (int i = 0; i < previous_layer_size; i++) {
-		sum += weights[i] * previous_layer[i]->getValue();
+		sum += weights[i] * 1.0;//previous_layer[i]->getValue();
 	}
 
 	// Apply the derivative of the sigmoid function
 	// e^x / (1 + e^x)^2
-	double test = exp(sum);
 
 	double denominator = 1 + exp(sum);
 	// Square it
