bbbc4c36873ef85e36ae147c6fc889de18ec5d61
Nicki Ogg <oggnicki@yahoo.com>
2015-05-19 23:50:41 +0200

    200 hn

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 
 	cout << "Training neural network" << endl;
 
-	NeuralNetwork *network = new NeuralNetwork(784, 10, 1, 10);
+	NeuralNetwork *network = new NeuralNetwork(784, 200, 1, 10);
 	network->print();
 
 	Image *testImage = training_images[0];
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
 
 	cout << "Training..." << endl;
 	int iterations = 0;
-	//while (error_rate > 0.1) {
+	while (error_rate > 0.1) {
 		cout << "Training iteration: " << iterations << ", Error rate: " << error_rate << endl;
 		// Perform a round of training
 		for (int i = 0; i < training_images.size(); i++) {
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
 		}
 		error_rate = getErrorRate(network, training_images);
 		iterations++;
-	//}
+	}
 
 	cout << "Final error rate: " << error_rate << endl;
 
